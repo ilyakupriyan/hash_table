8121478835f907b0c80e18c65a24ce02b3ad0798
Ilya Kupriyanov <ikuprianov423@gmail.com>
2024-01-25 19:13:09 +0300

    Added create and free function of hash table

---
+++ headers/hash.h
@@ -0,0 +1,30 @@
+#ifndef HASH_H
+#define HASH_H
+
+#define MAX_SIZE_KEY 256
+#define MAX_SIZE_MAP 500
+
+typedef struct ht_item ht_item;
+typedef struct table hashTable;
+
+//Node of hash map
+struct ht_item {
+    char *key;
+    char *value;
+};
+
+//Hash map
+struct table {
+    ht_item **items;
+    int size;
+    int count;
+};
+
+int         insertValue(hashTable*map, const char *key, const char *value);     //  insert key-value pair in hash table
+int         removeValue(hashTable *map, const char *value);                     //  remove value located in hash table
+char *      getValue(hashTable *map, const char *key);                          //  get value located in hash table           
+
+hashTable * createHashMap(int size);                                            //  create hash table
+void        deleteHash(hashTable *map);                                         //  remove hash table
+
+#endif
---
+++ src/hash.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <string.h>
+#include "../headers/hash.h"
+
+/*
+ * @brief               Create hashTable
+ * @param ht_size       Max count of item in hash table
+ */
+hashTable* createHashMap(int ht_size) 
+{
+    hashTable *table = (hashTable *) malloc(sizeof (hashTable *));
+    table->size = ht_size;
+    table->count = 0;
+
+    table->items = (ht_item**) calloc (ht_size, sizeof(ht_item));
+}
+
+/*
+ * @brief               Create item of hash table
+ * @param key           Key access to value
+ * @param value         Value corresponding to the key
+ */
+ht_item *createItem(char *key, char *value)
+{
+    ht_item *item = (ht_item *) malloc(sizeof(ht_item *));
+
+    item->key = (char *) malloc(strlen(key) + 1);
+    item->value = (char *) malloc(strlen(value) + 1);
+
+    strcpy(item->key, key);
+    strcpy(item->value, value);
+
+    return item;
+}
+
+/*
+ * @brief               Free memory after item in heap
+ * @param item              
+ */
+void free_item(ht_item *item)
+{
+    free(item->key);
+    free(item->value);
+
+    free(item);
+}
+
+/*
+ * @brief               Free memory after table in heap
+ * @param               Pointer to hash table
+ */
+void free_table(hashTable *table)
+{
+    for (int iter = 0; iter < table->size; iter++)
+    {
+        ht_item *item = table->items[iter];
+
+        if (item != NULL)
+            free_item(item);
+    }
+
+    free(table->items);
+    free(table);
+}
+
+/*
+ * @breaf               Insert key-value pair in hash table                     
+ * @param map           Pointer to hash table              
+ * @param key           Key access to value
+ * @param value         Value corresponding to the key
+ */
+int insertValue(hashTable *map, const char *key, const char *value)
+{
+    ht_item *new_item;
+}
+
+char *getValue(hashTable *map, const char *key)
+{
+    unsigned long index = hashFunction(key);
+
+    
+}
+
+/*
+ * @breaf               Translate key to hash value
+ * @param key           Key for translate to the hash value
+ */
+unsigned long hashFunction (char* key)
+{
+    unsigned long index = 0;
+    __uint8_t iter;
+
+    for (iter = 0; key[iter] != '\0' && iter < MAX_SIZE_KEY; iter++) {
+        index += key[iter];
+    }
+
+    index += iter;
+
+    return index % MAX_SIZE_MAP;
+}
+
+
